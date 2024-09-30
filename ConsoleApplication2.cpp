a257e60bfac9e9c307bb226fad718a9e03a6757e
HappyMan <100917359+NPChappyman@users.noreply.github.com>
2024-09-30 23:02:09 +0300

    -

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -2,14 +2,15 @@
 //
 
 #include <iostream>
-
-int main()
+void Swap(int* x, int* y)
 {
-    
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
-static void InsertionSort(int* a,int len)
+static void InsertionSort(int* a, int len)
 {
-    for (int i = 0; i < len-1; i++)
+    for (int i = 0; i < len - 1; i++)
     {
         for (int j = i + 1; j < len; j++)
         {
@@ -20,9 +21,17 @@ static void InsertionSort(int* a,int len)
         }
     }
 }
-void Swap(int* x, int* y)
+
+void print(int* a, int len)
 {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+    for (int i = 0; i < len; i++)
+    {
+        std::cout << a[i] << " ";
+    }
+}
+int main()
+{
+    int a[] = {1,22,1231,13132,212,17,12,7,45,32};
+    InsertionSort(a,10);
+    print(a,10);
 }
